0fd4c1b922fc4c790098756e426fa787821371e8
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-09 00:09:33 +0100

    0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+import sqlite3
+import functools
+
+# Decorator to log SQL queries
+def log_queries(my_funct):
+    @functools.wraps(my_funct)
+    def de_wrapper(*args, **kwargs):
+        # Log the SQL query
+        if 'kizito' in kwargs:
+            print(f"[LOG] Executing SQL: {kwargs['query']}")
+        elif len(args) > 0:
+            print(f"[LOG] Executing SQL: {args[0]}")
+        
+        # Call the actual function
+        return my_funct(*args, **kwargs)
+    return de_wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
