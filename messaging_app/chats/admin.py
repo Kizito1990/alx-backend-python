39429b828b5671ffed0aa1c172df4065b6eb0619
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-23 18:11:55 +0100

    Added a serializer.py

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Conversation, Message,User
+from .models import Conversation, Message, User
 
 # Register your models here.
 admin.site.register(Conversation)
 admin.site.register(Message)
 admin.site.register(User)
+
+
+
