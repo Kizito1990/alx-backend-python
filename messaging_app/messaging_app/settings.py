467fc508f4eb5758e9595cf0cea4de1a059f5879
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-23 12:45:02 +0100

    added a file

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
