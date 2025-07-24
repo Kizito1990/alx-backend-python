05a92ca586d97ac492b25b3f1251844bc8e61936
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-24 12:25:47 +0100

    added a permission.py file

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -43,9 +43,16 @@ class MessageViewSet(viewsets.ModelViewSet):
     permission_classes = [IsParticipantOfConversation]
 
     def get_queryset(self):
-        return Message.objects.filter(
-            conversation__participants=self.request.user
-        
-    
+        return Message.objects.filter(conversation__participants=self.request.user)
+
     def perform_create(self, serializer):
+        conversation_id = self.request.data.get("conversation")
+        try:
+            conversation = Conversation.objects.get(id=conversation_id)
+        except Conversation.DoesNotExist:
+            raise PermissionDenied(detail="Conversation does not exist", code=HTTP_403_FORBIDDEN)
+
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied(detail="You are not a participant of this conversation", code=HTTP_403_FORBIDDEN)
+
         serializer.save(sender=self.request.user)
