ac0033ef82396ad8d50dc3352ecf64a69ede5e32
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-24 18:54:09 +0100

    added Middleware.py

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'chats.middleware.RestrictAccessByTimeMiddleware',
     'chats.middleware.OffensiveLanguageMiddleware'
     'chats.middleware.RolePermissionMiddleware',  # ✅ Add here
+    
 
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
