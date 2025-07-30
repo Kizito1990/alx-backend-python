5e72e0b49b59bfa8a220befbd8b1bddf198f12be
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-30 12:46:30 +0100

    Added Models.py file

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -6,11 +6,23 @@ from .models import Message, Notification
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
-        
-class MessageHistory(models.Model):
-    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
-    old_content = models.TextField()
-    edited_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"History for message ID {self.message.id} at {self.edited_at}"
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.timezone import now
+from messaging.models import Message, MessageHistory
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message=old_message,
+                    old_content=old_message.content,
+                    edited_at=now()
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
