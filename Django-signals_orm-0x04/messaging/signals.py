7ee0ede13c52672d33e939f5b9108e3f52fa794e
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-30 12:35:40 +0100

    Added Models.py file

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -6,3 +6,11 @@ from .models import Message, Notification
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+        
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for message ID {self.message.id} at {self.edited_at}"
