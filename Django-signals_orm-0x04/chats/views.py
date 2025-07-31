8bacb45e190652dc2d96770146b1ad2c433a6d6a
Azegba Kizito Amandianeze <kizitoamandi96@gmail.com>
2025-07-31 13:11:23 +0100

    Added  messaging_app

--- Django-signals_orm-0x04/chats/views.py
+++ Django-signals_orm-0x04/chats/views.py
@@ -4,8 +4,17 @@ from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from .models import Message
+from django.views import View
 
 @cache_page(60)  # Caches the view for 60 seconds
 def message_list(request):
     messages = Message.objects.all().select_related('sender', 'receiver').order_by('-timestamp')
     return render(request, 'chats/message_list.html', {'messages': messages})
+
+
+@method_decorator(cache_page(60), name='dispatch')
+class MessageListView(View):
+    def get(self, request, *args, **kwargs):
+        messages = Message.objects.all().select_related('sender', 'receiver').order_by('-timestamp')
+        return render(request, 'chats/message_list.html', {'messages': messages})
+
